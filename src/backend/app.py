5d35db83a90b0c1a0c86abbe4ff6564b762bd2bd
Vhugala Mafanywa <142503635+VhugalaMafanywa@users.noreply.github.com>
2026-04-06 21:11:06 +0200

    Update app.py

--- src/backend/app.py
+++ src/backend/app.py
@@ -74,5 +74,4 @@ def handle_command():
 
 
 if __name__ == "__main__":
-    print("🚀 Flask server running on http://127.0.0.1:8000")
-    app.run(debug=True, port=8000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
